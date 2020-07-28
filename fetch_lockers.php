61e774a826fabb9d9be2979f84e81385bb46ffca
Ege Feyzioglu <egefeyzioglu@yahoo.com>
2020-07-28 15:47:23 +0300

    Resolved #9

--- fetch_lockers.php
+++ fetch_lockers.php
@@ -1,22 +1,35 @@
 <?php
-  // Dynamically fetch lockers for select menu by building and size selected
+  /* Dynamically fetch lockers for select menu by building and size selected
+   * Data returned in a JSON array
+   * If an invalid value is selected or no locker is found, an empty JSON array is returned
+   * Else, the available lockers are appended to a JSON array, after
+   * "Select Locker"
+   */
+
   session_start();
   require 'model/db.php';
 
   $sql = "SELECT * FROM `locker`
     WHERE `locker_size` = '" .mysqli_real_escape_string($conn,$_GET['size']). "'
-    AND `locker_location` = '" .mysqli_real_escape_string($conn,$_GET['location']). "'";
+    AND `locker_location` = '"
+                      .mysqli_real_escape_string($conn,$_GET['location']). "'
+    AND locker_status LIKE 'Available'";
+    //echo($sql);
 
   $result = mysqli_query($conn, $sql);
 
-  $output = 'option value="">Select Locker</option>';
+  $output = "";
+  $arr = array("Select Locker"); // This is a magic value.
+                                 // Please see the select.locker change event
+                                 // handler in footer.php
 
-  if($result) {
+  if($result && mysqli_num_rows($result) > 0) {
     while($row = mysqli_fetch_array($result)) {
-      $output .= '<option value="' .$row["locker_id"].'">'.$row["locker_id"].'</option>';
+      $arr[] = $row['locker_id'];
+      $output = json_encode($arr);
     }
   } else {
-    $output = 'option value="">None avabilable</option>';
+    $output = '[]';
   }
 
   echo $output;
--- footer.php
+++ footer.php
@@ -33,11 +33,13 @@
 
     // Initialize select list
     $('select').material_select();
+    $('.dropdown-trigger').dropdown();
 
     // Initialize datepicker
-    $('.datepicker').pickadate({
-      format: 'dd/mm/yy'
-    });
+    $('.datepicker').pickadate();
+    $('.datepicker').on('mousedown',function(event){
+        event.preventDefault();
+    })
 
     $('.tooltipped').tooltip();
 
@@ -61,6 +63,13 @@
         });
 
 
+  function setAnchorDisabled(anchor, disabled) {
+    //debugger;
+    if(disabled)
+      $(anchor).addClass("disabled");
+    else
+      $(anchor).removeClass("disabled");
+  }
   var location = "";
 
   // Set active location
@@ -69,20 +78,49 @@
   })
 
   // Get available lockers by size and building
-  $(".size").on('change', function() {
+  $("select.size").on('change', function() {
     var size = $(this).val();
+    var obj = this;
+    locker = $(obj).parent().parent().parent().parent().find('select.locker'); //The select object
+    //debugger;
+    //First check if anything has been selected
+    if(size == ""){ //If not, reset the locker and return
+      locker.html("<option value=''>Select Locker</option>");
+    } else {
+      $.ajax({
+        url: "fetch_lockers.php",
+        method: "GET",
+        data:{location:location,size:size},
+        dataType: "text",
+        success:function(data) {
+          debugger;
+          //Create HTML from returned data
+          var dataArr = JSON.parse(data);
+          var selectHTML = "";
+          if(dataArr[0] == "Select Locker" && dataArr.length == 1) //This is the magic value referenced in fetch_lockers.php.
+            dataArr = [];
+          for(var i = 0; i < dataArr.length; i++){
+            selectHTML += "<option>" + dataArr[i] + "</option>\n";
+          }
+          if(dataArr.length == 0){
+            selectHTML = "<option value=''>None avabilable</option>";
+          }
+          // Replace options of nearest locker selected
+          locker.html(selectHTML); //Fill the select object with the generated HTML
+          $('select').material_select(); //Since we changed the select, we need to initialize it again
+        }
+      });
+    }
+    $('select').material_select(); //Since we changed the select, we need to initialize it again
+    $(locker).change(); //Trigger the change event for the locker select so that it can update the button
+  });
 
-    $.ajax({
-      url: "fetch_lockers.php",
-      method: "GET",
-      data:{location:location,size:size},
-      dataType: "text",
-      success:function(data) {
-        // Replace options of nearest locker selected
-        console.log(data);
-        $(this).parent().next().find('.locker').html(data);
-      }
-    })
+  //When a locker is selected, set the book button
+  $('select.locker').on('change', function() {
+    var btn = $(this).parent().parent().parent().find("a.btn");
+    console.log(this.value);
+    setAnchorDisabled(btn, this.value == '');
+    btn[0].href = btn[0].href.substring(0,btn[0].href.lastIndexOf("=") + 1) + this.value;
   });
 
   });
