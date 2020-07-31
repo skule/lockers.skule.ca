8341376ff69919b0891bfe5f806acd37a8905ea0
Ege Feyzioglu <egefeyzioglu@yahoo.com>
2020-08-01 00:23:06 +0300

    Resolved #16

--- js/init.js
+++ js/init.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
   $('.button-collapse').sideNav();
+
+
+  //Highlight current item in sidebar
+  var path =  window.location.pathname.substring(window.location.pathname.lastIndexOf("/") + 1)
+  if(path == ""){
+    $("ul.side-nav:not(#nav-mobile) li a[href='index.php']").parent().addClass('active');
+    $("nav ul.nav-mobile li a[href='index.php']").parent().addClass('active');
+  }else{
+    $("ul.side-nav:not(#nav-mobile) li a[href='" + path + "']").parent().addClass('active');
+    $("nav ul.nav-mobile li a[href='" + path + "']").parent().addClass('active');
+  }
 });
